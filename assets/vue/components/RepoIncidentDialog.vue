f693077bd93f1898907a5f099fd5fe3e8c5d3157
Sebastian Riedel <s@kraih.com>
2022-01-17 15:25:24 +0100

    Fix modal close button

--- assets/vue/components/RepoIncidentDialog.vue
+++ assets/vue/components/RepoIncidentDialog.vue
@@ -11,9 +11,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="modal-title">Incidents for {{ title }}</h5>
-          <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <ul>
