8ff5c5b4c77f3ff8ad1f504fb818b3a0e3f7f758
Oliver Kurz <okurz@suse.de>
2026-01-17 12:10:11 +0100

    feat: generalize RequestLink to support multiple source types

--- assets/vue/components/RequestLink.vue
+++ assets/vue/components/RequestLink.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="incident-link">
-    <a :href="obsLink" target="_blank" class="rr-link">{{ incident.rr_number }}:{{ packageName }}</a>
+    <a :href="sourceUrl" target="_blank" class="rr-link">
+      <i :class="sourceIcon"></i>
+      {{ linkText }}
+    </a>
   </div>
 </template>
 
@@ -9,11 +12,23 @@ export default {
   name: 'RequestLink',
   props: {incident: {type: Object, required: true}},
   computed: {
-    obsLink() {
+    sourceUrl() {
+      if (this.incident.type === 'git' || !this.incident.rr_number) {
+        return this.incident.url;
+      }
       return `${this.appConfig.obsUrl}/request/show/${this.incident.rr_number}`;
     },
+    linkText() {
+      if (this.incident.rr_number) {
+        return `${this.incident.rr_number}:${this.packageName}`;
+      }
+      return this.packageName || 'Source';
+    },
+    sourceIcon() {
+      return this.incident.type === 'git' ? 'fas fa-code-branch' : 'fas fa-box-open';
+    },
     packageName() {
-      return this.incident.packages[0];
+      return this.incident.packages && this.incident.packages.length > 0 ? this.incident.packages[0] : '';
     }
   }
 };
